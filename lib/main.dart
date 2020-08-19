a766334f756d17ad139a608d99d2a6eac0c8c3db
ankith1998 <ajkarthik1315@gmail.com>
2020-08-20 00:35:04 +0530

    scroll and parameter listing enabled

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ void main()
   runApp(app());
 }
 class app extends StatelessWidget {
+  TextEditingController sname = TextEditingController();
+  TextEditingController sroll = TextEditingController();
+  TextEditingController sadd = TextEditingController();
+  TextEditingController scol = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,51 +17,69 @@ class app extends StatelessWidget {
         appBar: AppBar(
           title: Text("STUDENTS APP")
         ),
-        body: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              SizedBox(height: 120.0,),
-              Text("Name"),
-              TextField(decoration: InputDecoration(
-                hintText: "Enter student name",
-                prefixIcon: Icon(Icons.account_circle),
-                border: OutlineInputBorder()
-              ),
-              ),
-              SizedBox(height: 20.0,),
-              Text("Roll no"),
-              TextField(decoration: InputDecoration(
-                hintText: "Enter roll number",
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.account_circle)
-              ),),
-              SizedBox(height: 20.0,),
-              Text("Admission no"),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter admission number",
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.account_circle)
-                ),
-              ),
-              SizedBox(height: 20.0,),
-              Text("College"),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter college name",
-                  prefixIcon: Icon(Icons.school),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(height: 120.0,),
+                Text("Name"),
+                TextField(
+                  controller: sname,
+                  decoration: InputDecoration(
+                  hintText: "Enter student name",
+                  prefixIcon: Icon(Icons.account_circle),
                   border: OutlineInputBorder()
                 ),
-              ),
-              SizedBox(height: 20.0,),
-              RaisedButton(
-                onPressed: (){},
-                color: Colors.amber,
-                child: Text("SUMBIT"),
-              )
+                ),
+                SizedBox(height: 20.0,),
+                Text("Roll no"),
+                TextField(
+                  controller: sroll,
+                  decoration: InputDecoration(
+                  hintText: "Enter roll number",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.account_circle)
+                ),),
+                SizedBox(height: 20.0,),
+                Text("Admission no"),
+                TextField(
+                  controller: sadd,
+                  decoration: InputDecoration(
+                    hintText: "Enter admission number",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.account_circle)
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Text("College"),
+                TextField(
+                  controller: scol,
+                  decoration: InputDecoration(
+                    hintText: "Enter college name",
+                    prefixIcon: Icon(Icons.school),
+                    border: OutlineInputBorder()
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                RaisedButton(
+                  onPressed: (){
+                    var getname = sname.text;
+                    var getroll = sroll.text;
+                    var getadd = sadd.text;
+                    var getcol =scol.text;
+                    print(getname);
+                    print(getroll);
+                    print(getadd);
+                    print(getcol);
+                    print("SUCCESSFUL REGISTRATION");
+                  },
+                  color: Colors.amber,
+                  child: Text("SUMBIT"),
+                )
 
-            ],
+              ],
+            ),
           ),
         ),
       ),
